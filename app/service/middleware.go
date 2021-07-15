4f820db91614994b71ec0ac00db127fb90a1d72c
赵翔 <18671056539@163.com>
2021-07-16 00:29:16 +0800

    little change

--- app/service/middleware.go
+++ app/service/middleware.go
@@ -3,9 +3,10 @@ package service
 import (
 	"alfred/app/model"
 	"github.com/gogf/gf/net/ghttp"
+	"net/http"
 )
 
-var MiddleWare = new (serviceMiddleware)
+var MiddleWare = new(serviceMiddleware)
 
 type serviceMiddleware struct{}
 
@@ -16,24 +17,24 @@ func (s *serviceMiddleware) Ctx(r *ghttp.Request) {
 	Context.Init(r, customCtx)
 	if user := Session.GetUser(r.Context()); user != nil {
 		customCtx.User = &model.ContextUser{
-			Id:       		user.ID,
+			Id:             user.ID,
 			Identification: user.Identification,
-			Password: 		user.Password,
-			Nickname: 		user.NickName,
-			Permission:		user.Permission,
+			Password:       user.Password,
+			Nickname:       user.NickName,
+			Permission:     user.Permission,
 		}
 	}
 	r.Middleware.Next()
 }
 
-//// 鉴权中间件，只有登录成功之后才能通过
-//func (s *serviceMiddleware) Auth(r *ghttp.Request) {
-//	if User.IsSignedIn(r.Context()) {
-//		r.Middleware.Next()
-//	} else {
-//		r.Response.WriteStatus(http.StatusForbidden)
-//	}
-//}
+// 鉴权中间件，只有登录成功之后才能通过
+func (s *serviceMiddleware) Auth(r *ghttp.Request) {
+	if User.IsSignedIn(r.Context()) {
+		r.Middleware.Next()
+	} else {
+		r.Response.WriteStatus(http.StatusForbidden)
+	}
+}
 
 // 允许接口跨域请求
 func (s *serviceMiddleware) CORS(r *ghttp.Request) {
